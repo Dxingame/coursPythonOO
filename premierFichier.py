d48fb43d6c38f0a871a73db0ef7815726b9304b4
Jean-Rémi Pourtau <jean-remi.pourtau@viveris.fr>
2020-04-02 18:41:57 +0200

    Changement sur le fichier ==> tableau de quote et characters

--- premierFichier.py
+++ premierFichier.py
@@ -1,1 +1,18 @@
+# -*- coding: utf8 -*-
+
 print("yo, t'as pas un 06?")
+
+quotes = [
+    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
+    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+]
+
+characters = [
+    "alvin et les Chipmunks", 
+    "Babar", 
+    "betty boop", 
+    "calimero", 
+    "casper", 
+    "le chat potté", 
+    "Kirikou"
+]
