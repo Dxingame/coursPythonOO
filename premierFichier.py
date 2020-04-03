e0b3a2a4287a4a4bb813240b94331801e8f8312a
Jean-Rémi Pourtau <jean-remi.pourtau@viveris.fr>
2020-04-03 16:23:26 +0200

    Ajout des fichiers characters et quotes + premiere version generateur de citations

--- premierFichier.py
+++ premierFichier.py
@@ -1,18 +1,51 @@
 # -*- coding: utf8 -*-
 
-print("yo, t'as pas un 06?")
+import random
+import json
 
-quotes = [
-    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
-    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
-]
+listePersonnages = []
+listeCitations = []
+monPersonnage = ""
+maCitation = ""
 
-characters = [
-    "alvin et les Chipmunks", 
-    "Babar", 
-    "betty boop", 
-    "calimero", 
-    "casper", 
-    "le chat potté", 
-    "Kirikou"
-]
+# On veut faire dire à un personnage, une citation au hasard
+
+# Choix du personnage
+def choixPersonnage(liste):
+	nombreAleatoire = random.randint(0,len(liste)-1)
+	#nombreAleatoire = 1
+	return liste[nombreAleatoire]
+
+# Récupération de la citation au hasard
+def citationHasard(liste):
+	nombreAleatoire = random.randint(0,len(liste)-1)
+	#nombreAleatoire = 1
+	return liste[nombreAleatoire]
+
+# Récupération d'une liste
+def recupListeJSON(fichier, motclef):
+	valeurs=[]
+	with open(fichier) as f:
+		data = json.load(f)
+		for entry in data:
+			valeurs.append(entry[motclef])
+	return valeurs
+	
+# Ecrire du message
+def messageAffiche(personage,message):
+	return "{} a dit {}".format(personage,message)
+	
+
+reponseUtilisateur = input("Pour continuer pressez entrée, pour quitter pressez B")
+listePersonnages = recupListeJSON("characters.json","character")
+listeCitations = recupListeJSON("quotes.json","quote")
+#print(listeCitations)
+#print(listePersonnages)
+#Proposition personage
+while reponseUtilisateur != 'B':
+	monPersonnage = choixPersonnage(listePersonnages)
+#	print("Mon perso" + monPersonnage)
+	maCitation = citationHasard(listeCitations)
+#	print("ma citation" + maCitation)
+	print(messageAffiche(monPersonnage,maCitation))
+	reponseUtilisateur = input("Pour continuer pressez entrée, pour quitter pressez B")
